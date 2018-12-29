638ea80e8ff3aab8b55562270bb1b9d15ebda4c8
Michael P. Starkweather <mpstark@gmail.com>
2018-12-29 16:33:57 -0600

    First working prototype

---
+++ HideCareerModeDays/Patches.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Reflection;
+using BattleTech;
+using BattleTech.UI;
+using Harmony;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace HideCareerModeDays
+{
+    [HarmonyPatch(typeof(SGHeaderWidget), "RefreshCountdown")]
+    public static class SGHeaderWidget_RefreshCountdown_Patch
+    {
+        public static void Postfix(SGHeaderWidget __instance)
+        {
+            var simGame = Traverse.Create(__instance).Field("simState").GetValue<SimGameState>();
+            var careerModeArea = Traverse.Create(__instance).Field("careerModeArea").GetValue<GameObject>();
+            
+            if (simGame.IsCareerMode() && (Patches.Settings.HideAlways || simGame.GetCareerModeDaysRemaining() < 0))
+            {
+                careerModeArea.SetActive(false);
+            }
+        }
+    }
+
+    public class ModSettings
+    {
+        public bool HideAlways = false;
+    }
+
+    public static class Patches
+    {
+        public static ModSettings Settings;
+
+        public static void Init(string modDir, string modSettings)
+        {
+            var harmony = HarmonyInstance.Create("io.github.mpstark.HideCareerModeDays");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            // read settings
+            try
+            {
+                Settings = JsonConvert.DeserializeObject<ModSettings>(modSettings);
+            }
+            catch (Exception)
+            {
+                Settings = new ModSettings();
+            }
+        }
+    }
+}
